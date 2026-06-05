579655b7d143181bec7dd23c58698d390952849f
Norbert Varga <norbert.varga@delaware.pro>
2026-06-05 13:35:51 +0000

    fix

--- app/ui5/webapp/controller/View1.controller.js
+++ app/ui5/webapp/controller/View1.controller.js
@@ -1,20 +1,73 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast"
-], (Controller, MessageToast) => {
+], (Controller, JSONModel, MessageToast) => {
     "use strict";
 
     return Controller.extend("main.cap.service.ui.ui5.controller.View1", {
         onInit() {
+
+            const params = new URLSearchParams(window.location.search);
+
+                this.bp = params.get("bp");
+            console.log("Business Partner from URL:", this.bp);
+                // ✅ View model (instead of DOM manipulation)
+                const oViewModel = new JSONModel({
+                BankAccounts: [],
+                selected: null,
+                message: ""
+                });
+
+                this.getView().setModel(oViewModel, "viewModel");
+          
+                this.getView().attachEventOnce("modelContextChange", this.checkInit.bind(this));
+
+
+            /*    const fnCheck = () => {
+                    const oModel = this.getView().getModel();
+                    if (oModel) {
+                        this.checkInit();
+                    } else {
+                        setTimeout(fnCheck, 50);
+                    }
+                };
+                fnCheck();*/
+
+
         },
+
+         async checkInit(oEvent) {
+           try{ 
+
+                const oModel = this.getView().getModel(); // OData V4 model
+
+                const oAction = oModel.bindContext("/checkConnection(...)");
+
+                oAction.setParameter("sample1", "first param");
+                oAction.setParameter("sample2", "second param");
+                await oAction.execute();
+                const data = oAction.getBoundContext().getObject();
+
+                const results = data.value || [];
+
+                console.log("Init task successful:", data);
+                MessageToast.show(results);
+            } catch (err) {
+                console.error("Error:", err);
+                MessageToast.show(err);
+            }
+
+        },
+
         async onCheckConnection1(oEvent) {
              try{ 
-
+                const uid = this.byId("inputSample").getValue();;
                 const oModel = this.getView().getModel(); // OData V4 model
 
                 const oAction = oModel.bindContext("/checkC4CConnection(...)");
 
-                oAction.setParameter("caseuuid", "asd123asd123");
+                oAction.setParameter("caseuuid", uid);
                 await oAction.execute();
                 const data = oAction.getBoundContext().getObject();
 
@@ -50,6 +103,5 @@ sap.ui.define([
             }
 
         }
-
     });
 });
--- srv/main-service.js
+++ srv/main-service.js
@@ -2,23 +2,25 @@ const cds = require('@sap/cds');
 const { executeHttpRequest } = require('@sap-cloud-sdk/http-client');
 const { getDestination } = require('@sap-cloud-sdk/connectivity');
 
-module.exports = async function () {
+module.exports = class DatabaseService extends cds.ApplicationService {
+async init() {
 
   // S4 via CAP
    this.on('checkConnection', async (req) => {
     const { sample1, sample2 } = req.data;
     console.log("cap received: " + sample1 + ", " + sample2);
-    return "Hello from CAP: " + sample1 + ", " + sample2;
+    return "This is init: " + sample1 + ", " + sample2;
 
    });
    this.on('checkC4CConnection', async (req) => {
     const { caseuuid } = req.data;
+    const url =`sap/c4c/api/v1/case-service/cases/${caseuuid}`;
     try{
             const caseResponse = await executeHttpRequest(
                 { destinationName: "c4c_dev" },
                 {
                     method: "GET",
-                    url: `sap/c4c/api/v1/case-service/cases/${caseuuid}`,
+                    url: url,
                     headers: {
                       "Accept": "application/json"
                     }                
@@ -34,4 +36,7 @@ module.exports = async function () {
 
    });
    
+ 
+  await super.init();
+  }
 }
